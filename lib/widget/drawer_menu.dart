94efc991a5204864f74dc95b0365abec442c121e
Shtirlits1972 <shtirlits1972@gmail.com>
2025-03-02 07:52:22 +0200

    fix driver

--- lib/widget/drawer_menu.dart
+++ lib/widget/drawer_menu.dart
@@ -10,98 +10,96 @@ class DrawerMenu extends StatefulWidget {
 class _DrawerMenuState extends State<DrawerMenu> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Menu'),
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
             ),
-            ListTile(
-              title: const Text('Makes'),
-              onTap: () {
-                Navigator.pushNamed(context, '/MakesForm');
-              },
-            ),
-            const Divider(
-              color: Colors.grey,
-              thickness: 1,
-            ),
-            ListTile(
-              title: const Text('Car Models'),
-              onTap: () {
-                Navigator.pushNamed(context, '/CarModelsForm');
-              },
-            ),
-            const Divider(
-              color: Colors.grey,
-              thickness: 1,
-            ),
-            ListTile(
-              title: const Text('Trims'),
-              onTap: () {
-                Navigator.pushNamed(context, '/TrimsForm');
-              },
-            ),
-            const Divider(
-              color: Colors.grey,
-              thickness: 1,
-            ),
-            ListTile(
-              title: const Text('Engine'),
-              onTap: () {
-                Navigator.pushNamed(context, '/EngineForm');
-              },
-            ),
-            const Divider(
-              color: Colors.grey,
-              thickness: 1,
-            ),
-            ListTile(
-              title: const Text('Body'),
-              onTap: () {
-                Navigator.pushNamed(context, '/BodyForm');
-              },
-            ),
-            const Divider(
-              color: Colors.grey,
-              thickness: 1,
-            ),
-            ListTile(
-              title: const Text('Vin Model'),
-              onTap: () {
-                Navigator.pushNamed(context, '/VinModelForm');
-              },
-            ),
-            const Divider(
-              color: Colors.grey,
-              thickness: 1,
-            ),
+            child: Text('Menu'),
+          ),
+          ListTile(
+            title: const Text('Makes'),
+            onTap: () {
+              Navigator.pushNamed(context, '/MakesForm');
+            },
+          ),
+          const Divider(
+            color: Colors.grey,
+            thickness: 1,
+          ),
+          ListTile(
+            title: const Text('Car Models'),
+            onTap: () {
+              Navigator.pushNamed(context, '/CarModelsForm');
+            },
+          ),
+          const Divider(
+            color: Colors.grey,
+            thickness: 1,
+          ),
+          ListTile(
+            title: const Text('Trims'),
+            onTap: () {
+              Navigator.pushNamed(context, '/TrimsForm');
+            },
+          ),
+          const Divider(
+            color: Colors.grey,
+            thickness: 1,
+          ),
+          ListTile(
+            title: const Text('Engine'),
+            onTap: () {
+              Navigator.pushNamed(context, '/EngineForm');
+            },
+          ),
+          const Divider(
+            color: Colors.grey,
+            thickness: 1,
+          ),
+          ListTile(
+            title: const Text('Body'),
+            onTap: () {
+              Navigator.pushNamed(context, '/BodyForm');
+            },
+          ),
+          const Divider(
+            color: Colors.grey,
+            thickness: 1,
+          ),
+          ListTile(
+            title: const Text('Vin Model'),
+            onTap: () {
+              Navigator.pushNamed(context, '/VinModelForm');
+            },
+          ),
+          const Divider(
+            color: Colors.grey,
+            thickness: 1,
+          ),
 
-            ListTile(
-              title: const Text('Setting'),
-              onTap: () {
-                Navigator.pushNamed(context, '/SettingsPage');
-              },
-            ),
-            const Divider(
-              color: Colors.grey,
-              thickness: 1,
-            ),
-            //
+          ListTile(
+            title: const Text('Setting'),
+            onTap: () {
+              Navigator.pushNamed(context, '/SettingsPage');
+            },
+          ),
+          const Divider(
+            color: Colors.grey,
+            thickness: 1,
+          ),
+          //
 
-            ListTile(
-              title: const Text('Cancel'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
+          ListTile(
+            title: const Text('Cancel'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
     );
   }
